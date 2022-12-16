554ec348d02ee8c3489bcdbb7454e64b5269a867
vigneshc98 <vignesh.c1698@gmail.com>
2022-12-17 01:27:59 +0530

    vs:15C

--- ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
+++ ssc-brewery/src/main/java/guru/sfg/brewery/repositories/security/UserRepository.java
@@ -3,8 +3,13 @@ package guru.sfg.brewery.repositories.security;
 import guru.sfg.brewery.domain.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    //find locked accounts
+    List<User> findAllByAccountNonLockedAndLastModifiedDateIsBefore(Boolean locked, Timestamp timestamp);
 }
